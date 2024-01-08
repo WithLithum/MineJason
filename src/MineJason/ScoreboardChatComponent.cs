1ba04f25df5082e4ce5d1e2152639780792ee1b2
WithLithum <WithLithum@outlook.com>
2024-01-08 11:22:32 +0800

    chore(component): Add equality comparison support

--- src/MineJason/ScoreboardChatComponent.cs
+++ src/MineJason/ScoreboardChatComponent.cs
@@ -1,6 +1,11 @@
-﻿namespace MineJason;
+﻿// Copyright (c) WithLithum & contributors 2023-2024. All rights reserved.
+// Licensed under the GNU Lesser General Public License, either version 3 or
+// (at your opinion) any later version.
+
+namespace MineJason;
 
 using System.Text.Json.Serialization;
+using JetBrains.Annotations;
 using MineJason.Data;
 
 /// <summary>
@@ -8,13 +13,21 @@ using MineJason.Data;
 /// objective upon being presented to the user.
 /// </summary>
 /// <param name="score">The scoreboard details.</param>
-public sealed class ScoreboardChatComponent(ScoreboardSearcher score) : ChatComponent("score")
+[PublicAPI]
+public sealed class ScoreboardChatComponent(ScoreboardSearcher score) : ChatComponent("score"),
+    IEquatable<ScoreboardChatComponent>
 {
     /// <summary>
     /// Gets the scoreboard details.
     /// </summary>
     [JsonPropertyName("score")]
     public ScoreboardSearcher Score { get; } = score;
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"score={{name={Score.Name},objective={Score.Objective},value={Score.Value ?? "null"}}}";
+    }
 
     /// <inheritdoc />
     public override bool Equals(ChatComponent? other)
@@ -24,11 +37,32 @@ public sealed class ScoreboardChatComponent(ScoreboardSearcher score) : ChatComp
                && other is ScoreboardChatComponent component 
                && Score.Equals(component.Score);
     }
-
+    
     /// <inheritdoc />
-    public override string ToString()
+    public override bool Equals(object? obj)
     {
-        return $"score={{name={Score.Name},objective={Score.Objective},value={Score.Value ?? "null"}}}";
+        if (obj is ChatComponent component)
+        {
+            return Equals(component);
+        }
+        
+        return ReferenceEquals(this, obj) || obj is ScoreboardChatComponent other && Equals(other);
+    }
+    
+    /// <inheritdoc />
+    public bool Equals(ScoreboardChatComponent? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return base.Equals(other) && Score.Equals(other.Score);
     }
 
     /// <inheritdoc />
