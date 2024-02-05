2c6c97a2b140c861ee59392c96ef67a905ce294c
WithLithum <withlithum@outlook.com>
2024-02-05 12:05:04 +0800

    feat(selector): 增加玩家进度筛选的模型类型
    Refs: #7
    Signed-off-by: WithLithum <WithLithum@outlook.com>

---
+++ src/MineJason.Tests/Models/AdvancementConditionTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) WithLithum & contributors 2023-2024. All rights reserved.
+// Licensed under the GNU Lesser General Public License, either version 3 or
+// (at your opinion) any later version.
+
+namespace MineJason.Tests.Models;
+
+using MineJason.Data.Selectors.Advancements;
+
+public class AdvancementConditionTests
+{
+    [Test]
+    public void CriterionRule_Parse_Valid()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(CriterionRule.TryParse("criterion=true", out var result));
+            Assert.That(result, Is.EqualTo(new CriterionRule("criterion", true)));
+        });
+    }
+    
+    [Test]
+    public void CriterionRule_Parse_CaseSensitiveValue()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(!CriterionRule.TryParse("criterion=TrUe", out _));
+        });
+    }
+
+    [Test]
+    public void CriterionCondition_Parse_SuccessOnValid()
+    {
+        Assert.That(CriterionAdvancementCondition.TryParse("rigid=true,none=false", out _));
+    }
+    
+    [Test]
+    public void CriterionCondition_Parse_FailOnInvalid()
+    {
+        Assert.That(!CriterionAdvancementCondition.TryParse("rigid=true,,none=false", out _));
+    }
+    
+    [Test]
+    public void CriterionCondition_Parse_ValidValue()
+    {
+        const string parseValue = "rigid=true,none=false";
+        
+        if (!CriterionAdvancementCondition.TryParse(parseValue, out var result))
+        {
+            Assert.Fail("Parsing failed utterly. Check parser");
+            return;
+        }
+        
+        Assert.That(result.ToString(),
+            Is.EqualTo(parseValue));
+    }
+}
---
+++ src/MineJason/Data/Selectors/Advancements/CriterionAdvancementCondition.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) WithLithum & contributors 2023-2024. All rights reserved.
+// Licensed under the GNU Lesser General Public License, either version 3 or
+// (at your opinion) any later version.
+
+namespace MineJason.Data.Selectors.Advancements;
+
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using JetBrains.Annotations;
+
+/// <summary>
+/// Represents a criterion advancement condition.
+/// </summary>
+public sealed class CriterionAdvancementCondition : Collection<CriterionRule>,
+    IAdvancementCondition
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        var first = false;
+        
+        foreach (var value in this)
+        {
+            if (first)
+            {
+                builder.Append(',');
+            }
+            
+            builder.Append(value);
+            first = true;
+        }
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Attempts to parse the specified string representation to its instance representation.
+    /// </summary>
+    /// <param name="from">The string to parse.</param>
+    /// <param name="result">The parse result.</param>
+    /// <returns><see langword="true"/> if the syntax is correct; otherwise, <see langword="false"/>.</returns>
+    public static bool TryParse(string from, [NotNullWhen(true)] out CriterionAdvancementCondition? result)
+    {
+        var split = from.Split(',');
+
+        result = null;
+        
+        if (split.Length < 1)
+        {
+            // No conditions... Do not allow this be valid. FAIL.
+            return false;
+        }
+
+        result = new CriterionAdvancementCondition();
+        
+        foreach (var set in split)
+        {
+            if (!CriterionRule.TryParse(set, out var rule))
+            {
+                // FAIL: rule invalid.
+                result = null;
+                return false;
+            }
+            
+            result.Add(rule);
+        }
+
+        return true;
+    }
+}
